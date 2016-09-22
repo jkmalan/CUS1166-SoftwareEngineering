2c838fee09339746de14c6ac9d61ff7a265fc3a8
jkmalan <jkmalan@admincmd.com>
2016-09-22 13:14:38 -0400

    [ADD] Complete BankServer assignment

--- SocketProject/src/com/jkmalan/BankServer/BankClient.java
+++ SocketProject/src/com/jkmalan/BankServer/BankClient.java
@@ -1,9 +1,38 @@
 package com.jkmalan.BankServer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class BankClient {
 
     public static void connect() {
+        try {
+            Socket socket = new Socket("127.0.0.1", 8008);
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
 
+            Scanner input = new Scanner(is);
+            PrintWriter output = new PrintWriter(os);
+
+            Scanner user = new Scanner(System.in);
+            System.out.println("Please input a command or type 'help' for help...");
+            String expression = user.nextLine();
+            output.println(expression);
+            output.flush();
+
+            while (input.hasNextLine()) {
+                System.out.println(input.nextLine());
+            }
+
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("Something went wrong!");
+            e.printStackTrace();
+        }
     }
 
 }
--- SocketProject/src/com/jkmalan/BankServer/BankServer.java
+++ SocketProject/src/com/jkmalan/BankServer/BankServer.java
@@ -17,7 +17,9 @@ public class BankServer implements Runnable {
 
         try {
             ServerSocket server = new ServerSocket(8008);
-            while (true) {
+
+            boolean killServer = false;
+            while (!killServer) {
                 Socket client = server.accept();
 
                 InputStream is = client.getInputStream();
@@ -25,24 +27,65 @@ public class BankServer implements Runnable {
 
                 Scanner input = new Scanner(is);
                 String command = input.nextLine();
+                String[] args = command.split(" ");
 
-                // Handle data input
-                String result = "";
-                if (command.equalsIgnoreCase("create")) {
-
-                } else if (command.equalsIgnoreCase("deposit")) {
-
-                } else if (command.equalsIgnoreCase("withdraw")) {
-
-                } else if (command.equalsIgnoreCase("balance")) {
-
-                } else {
-
+                String result;
+                try {
+                    if (args.length == 2 && args[0].equalsIgnoreCase("balance")) {
+                        if (args[1].equalsIgnoreCase("*")) {
+                            result = "- - - - - Accounts - - - - -";
+                            for (Map.Entry<String, Double> account : accounts.entrySet()) {
+                                result += "\n" + account.getKey() + ": $" + account.getValue();
+                            }
+                        } else if (accounts.containsKey(args[1])) {
+                            result = "The balance for account [" + args[1] + "] is $" + accounts.get(args[1]).toString();
+                        } else {
+                            result = "The specified account does not exist!";
+                        }
+                    } else if (args.length == 3 && args[0].equalsIgnoreCase("deposit")) {
+                        if (accounts.containsKey(args[1])) {
+                            double temp = accounts.get(args[1]) + Double.parseDouble(args[2]);
+                            accounts.put(args[1], temp);
+                            result = "The balance for account [" + args[1] + "] is $" + accounts.get(args[1]).toString();
+                        } else {
+                            result = "The specified account does not exist!";
+                        }
+                    } else if (args.length == 3 && args[0].equalsIgnoreCase("withdraw")) {
+                        if (accounts.containsKey(args[1])) {
+                            double temp = accounts.get(args[1]) - Double.parseDouble(args[2]);
+                            if (temp >= 0) {
+                                accounts.put(args[1], temp);
+                                result = "The balance for account [" + args[1] + "] is $" + accounts.get(args[1]).toString();
+                            } else {
+                                result = "The specified amount exceeds available funds!";
+                            }
+                        } else {
+                            result = "The specified account does not exist!";
+                        }
+                    } else if (args.length == 3 && args[0].equalsIgnoreCase("create")) {
+                        if (!accounts.containsKey(args[1])) {
+                            accounts.put(args[1], Double.parseDouble(args[2]));
+                            result = "The balance for account [" + args[1] + "] is $" + accounts.get(args[1]).toString();
+                        } else {
+                            result = "The specified account already exists!";
+                        }
+                    } else if (args.length > 0 && args[0].equalsIgnoreCase("kill")) {
+                        result = "Killing the server process...";
+                        killServer = true;
+                    } else {
+                        result = "- - - - - Help Menu - - - - -" +
+                                "\n balance [account]" +
+                                "\n deposit [account] [amount]" +
+                                "\n withdraw [account] [amount]" +
+                                "\n create [account] [amount]";
+                    }
+                } catch (NumberFormatException e) {
+                    result = "Invalid argument! Please make sure the amount argument is a number!";
                 }
 
                 // Handle data output
                 PrintWriter output = new PrintWriter(os);
-                output.println(result);
+                output.println(result + "\n");
                 output.flush();
 
                 client.close();
